344af08a996f6e27db3d9c6b3dfb5885650cadc0
samerlutfi <samerlutfi96@gmial.com>
2021-06-20 18:11:09 +0300

    files created

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let userName = prompt("Hello there! whats your pretty name?");
+console.log(userName);
+let userAge = prompt("hi " + userName + " ! how old are you?");
+console.log(userAge);
+let origin = prompt(userAge +" mashallah<3 "+" where are you from?");
+console.log(origin);
+let favmus = prompt("i'm from "+ origin +" too,what's your favorite music type");
+console.log(favmus);
+alert("Nice "+userName+" i used to play some "+favmus +" songs when i where in "+ origin);
+
