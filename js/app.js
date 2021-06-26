e17e02982ad2e75ec63e5b77a9e429392bb13f56
samerlutfi <samerlutfi96@gmial.com>
2021-06-26 20:10:48 +0300

    files edited

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 let userName = prompt("Hello there! whats your pretty name?");
+alert('beautiful name')
 console.log(userName);
 let userAge = prompt("hi " + userName + " ! how old are you?");
+alert(userAge +" mashallah<3 ")
 console.log(userAge);
-let origin = prompt(userAge +" mashallah<3 "+" where are you from?");
+let origin = prompt(" where are you from?");
+alert('it is a wonderful country! i have been there last year')
 console.log(origin);
-let favmus = prompt("i'm from "+ origin +" too,what's your favorite music type");
+let favmus = prompt('what is your favorite music type?');
+alert('great chioce')
 console.log(favmus);
-alert("Nice "+userName+" i used to play some "+favmus +" songs when i was in "+ origin);
+alert("Nice "+userName+" i used to play some "+favmus +"  songs when i was in "+ origin);
 
